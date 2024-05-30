eaae111f0207b8e21d7303a34a608fba4a760bff
BanjoSamuelOladele <o.samuel@native.semicolon.africa>
2024-05-30 19:36:26 +0100

    testing

--- test/governance/GovernanceTest.sol
+++ test/governance/GovernanceTest.sol
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.25;
 import {Test, console} from "forge-std/Test.sol";
-import "src/governance/RogueGovernance.sol";
-import "src/governance/GovernanceToken.sol";
+import {RogueGovernance} from "src/governance/RogueGovernance.sol";
+import {RogueGovernanceToken} from "src/governance/GovernanceToken.sol";
 
 contract GovernanceTest is Test {
 
@@ -19,24 +19,13 @@ contract GovernanceTest is Test {
     }
 
     function testCreateProposal() external {
-        vm.startPrank(owner);
+        // vm.startPrank(owner);
 
         governance.createProposal("proposal to change the minimum proposal token balance", 1000, 30 minutes, RogueGovernance.ExecutionType.Staking_Amount);
-        // uint256 id;
-        // string description;
-        // uint256 newValue;
-        // uint256 endTime;
-        // Decision decision;
-        // uint256 forVote;
-        // uint256 againstVote;
-        // uint256 abstainVote;
-        // bool executed;
-        // ExecutionType executionType;
-        // mapping(address => bool) hasvoted;
 
-        (,string memory desc,,,,,,,,) = governance.proposals(1);
-
-        assertEq(desc, "proposal to change the minimum proposal token balance");
+        (,,,,,,,,,,) = governance.proposals(1);
+        console.log("here is the output","");
+        // assertEq(governance.proposalCount, 1);
     }
 
 }
